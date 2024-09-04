fde776ad9874dd9d54d939db6ace7dc39c842448
Jonatan Cardozo <futjona456@gmail.com>
2024-09-04 02:24:03 -0300

    v2

--- app.js
+++ app.js
@@ -1,47 +1,89 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dbconnect = require('./config'); 
 const ModelUser = require('./userModel');
 const app = express();
-
 const router = express.Router();
 
-router.post("/", async (req,res) =>{
-    const body = req.body;
-    const respuesta = await ModelUser.create(body)
-    res.send(respuesta)
-})
-
-router.get("/",async (req,res) =>{
-    const respuesta = await ModelUser.find({})
-    res.send(respuesta);
-})
-
-
-router.get("/:id",async (req,res) =>{
-    const id = req.params.id;
-    const respuesta = await ModelUser.findById(id)
-    res.send(respuesta);
-})
-
-router.put("/:id",async (req,res) =>{
-    const body = req.body
-    const id = req.params.id;
-    const respuesta = await ModelUser.findOneAndUpdate({_id:id},body)
-    res.send(respuesta);
-})
-
-router.delete("/:id",async (req,res) =>{
-    const id = req.params.id;
-    const respuesta = await ModelUser.deleteOne({_id:id})
-    res.send(respuesta);
-})
-
-
-app.use(express.json())
-app.use(router)
-app.listen(3001, () => {
-  console.log('El servidor está en el puerto 3001');
+router.post("/", async (req, res) => {
+    try {
+        const body = req.body;
+        const respuesta = await ModelUser.create(body);
+        res.send(respuesta);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
+router.get("/", async (req, res) => {
+    try {
+        const respuesta = await ModelUser.find({});
+        res.send(respuesta);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send('Invalid ID');
+    }
+    
+    try {
+        const respuesta = await ModelUser.findById(id);
+        if (!respuesta) {
+            return res.status(404).send('User not found');
+        }
+        res.send(respuesta);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    const id = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send('Invalid ID');
+    }
+
+    try {
+        const body = req.body;
+        const respuesta = await ModelUser.findOneAndUpdate({_id: id}, body, { new: true });
+        if (!respuesta) {
+            return res.status(404).send('User not found');
+        }
+        res.send(respuesta);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    const id = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send('Invalid ID');
+    }
+
+    try {
+        const respuesta = await ModelUser.deleteOne({_id: id});
+        if (respuesta.deletedCount === 0) {
+            return res.status(404).send('User not found');
+        }
+        res.send({ message: 'User deleted successfully' });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.use(express.json());
+app.use(router);
+
+app.listen(3001, () => {
+    console.log('El servidor está en el puerto 3001');
+});
 
 dbconnect();
